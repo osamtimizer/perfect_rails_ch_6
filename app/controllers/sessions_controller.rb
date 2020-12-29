580b57fdf407e12dafbd9a43ad287ae1a510ecf9
tokky <otokita@pepabo.com>
2020-12-29 15:30:39 +0900

    add github login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+    # find_or_create_from_auth_hash! will be implemented in user class.
+    user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "ログインしました"
+  end
+
+  def destroy
+    # reset_session will remove all sessions corresponding current user.
+    reset_session
+    redirect_to root_path, notice: "ログアウトしました"
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash!(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    nickname = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
+
+    # find_or_create_by! is implemented in Active Record.
+    User.find_or_create_by!(provider: provider, uid: uid) do |user|
+      user.name = nickname
+      user.image_url = image_url
+    end
+  end
+end
