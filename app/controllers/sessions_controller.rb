a4c62ddc7624c6b6e3457bde4cd3a9dc0de74744
tokky <otokita@pepabo.com>
2020-12-29 17:12:31 +0900

    add event actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate, only: :create
+
   def create
     # find_or_create_from_auth_hash! will be implemented in user class.
     user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
